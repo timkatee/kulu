4403dc7bd533fc39a1ae70eb4246e46e55b99cc7
Timothy Katee <kate2timo@gmail.com>
2021-11-04 22:08:03 +0900

    Updated base model to use record payload id if where clause is empty for single fetches...

--- src/components/base/BaseDbModel.js
+++ src/components/base/BaseDbModel.js
@@ -1,34 +1,41 @@
 const {Op} = require("sequelize");
 let db = require('../../database/models')
 
-class BaseDbModel{
+class BaseDbModel {
     //
     modelName = ""
     queryOptions = {}
-    recordPayload = {}
+    recordPayload = {id: null}
+
     //
     constructor(model_name) {
         this.modelName = model_name
     }
+
     //
-    async create(){
+    async create() {
         return db[this.modelName].create(this.recordPayload).catch((err) => err);
     }
+
     //
-    async read(mode='all'){
-        if(mode === 'single') {
+    async read(mode = 'all') {
+        if (mode === 'single') {
+            if (Object.keys(this.queryOptions).length === 0) // useful for update mutations
+                this.queryOptions.where = {id: this.recordPayload.id}
             return db[this.modelName].findOne(this.queryOptions).catch((err) => err);
-        }else{
+        } else {
             return db[this.modelName].findAll(this.queryOptions).catch((err) => err);
         }
     }
+
     //
-    async update(){
+    async update() {
         let item = await this.read('single')
         return item.update(this.recordPayload)
     }
+
     //
-    async delete(){
+    async delete() {
         return db[this.modelName].destroy(this.queryOptions).catch((err) => err);
     }
 
