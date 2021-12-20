c9b47dad0f01570ae5552e684342a2855858d69f
Timothy Katee <kate2timo@gmail.com>
2021-12-20 17:06:48 +0900

    Updated kulu to use new init-models files generated by sequelize-auto...

--- src/components/base/BaseDbModel.js
+++ src/components/base/BaseDbModel.js
@@ -1,12 +1,12 @@
 const {Op} = require("sequelize");
 let db = require('../../database/models')
+let initModels = require('../../database/models/init-models')(db.sequelize)
 
 class BaseDbModel {
     //
     modelName = ""
     queryOptions = {limit: 25}
     recordPayload = {id: null}
-
     //
     constructor(model_name) {
         this.modelName = model_name
@@ -14,7 +14,7 @@ class BaseDbModel {
 
     //
     async create() {
-        return db[this.modelName].create(this.recordPayload).catch((err) => err);
+        return initModels[this.modelName].create(this.recordPayload).catch((err) => err);
     }
 
     //
@@ -22,9 +22,9 @@ class BaseDbModel {
         if (mode === 'single') {
             if (!('where' in this.queryOptions)) // useful for update mutations which don't have 'other' wheres
                 this.queryOptions.where = {id: this.recordPayload.id}
-            return db[this.modelName].findOne(this.queryOptions).catch((err) => err);
+            return initModels[this.modelName].findOne(this.queryOptions).catch((err) => err);
         } else {
-            return db[this.modelName].findAll(this.queryOptions).catch((err) => err);
+            return initModels[this.modelName].findAll(this.queryOptions).catch((err) => err);
         }
     }
 
@@ -36,17 +36,17 @@ class BaseDbModel {
 
     //
     async delete() {
-        return db[this.modelName].destroy(this.queryOptions).catch((err) => err);
+        return initModels[this.modelName].destroy(this.queryOptions).catch((err) => err);
     }
 
     //
     async rawQuery(query, options) {
-        return db.sequelize.query(query, {model: db[this.modelName], ...options}).catch((err) => err)
+        return initModels.sequelize.query(query, {model: db[this.modelName], ...options}).catch((err) => err)
     }
 
     // useful for instances where use of the actual sequelize model instance is needed
     modelInstance() {
-        return db[this.modelName]
+        return initModels[this.modelName]
     }
 
 }
