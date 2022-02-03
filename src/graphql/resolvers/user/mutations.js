231a4f02143633e7b306ba188aee773e28abc6d9
Timothy Katee <kate2timo@gmail.com>
2022-02-03 14:36:38 +0900

    Updated resolve to take care of create and update on onr function...

--- src/graphql/resolvers/user/mutations.js
+++ src/graphql/resolvers/user/mutations.js
@@ -2,19 +2,10 @@ let GraphQLDb = require("../../../components/commons/GraphQLDb")
 let graphQlDb = new GraphQLDb('Users')
 
 module.exports = {
-    createUser : async(parent, args, context, info) => {
+    createEditUser: async (parent, args, context, info) => {
         //
         graphQlDb.recordPayload = args.user
         //
-        return await graphQlDb.mutation("CREATE")
-    },
-    updateUser : async(parent, args, context, info) => {
-        //
-        graphQlDb.recordPayload = args.user
-        //
-        return await graphQlDb.mutation("UPDATE")
-    },
-    // Users : async(parent, args, context, info) => {
-    //     return await graphQlDb.query(parent, args, context, info,'all')
-    // }
+        return await graphQlDb.mutation(args.type)
+    }
 }
