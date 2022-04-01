771f8b5d81cbdf7b86b21913e202f32c2de4f645
Timothy Katee <kate2timo@gmail.com>
2022-04-02 01:13:16 +0900

    Added more env variables to sequelize...

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {...config,...{host:process.env}});
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {...config,...{host:process.env.DB_HOST}});
 
 
 fs
