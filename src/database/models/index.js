08408ff666886ecb662ad6dcc76e5f5a5ab6d2ed
Timothy Katee <kate2timo@gmail.com>
2022-04-02 01:00:23 +0900

    Updated sequelize to use env variables for database details...

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -8,12 +8,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {...config,...{host:process.env.DB_HOST}});
+
 
 fs
   .readdirSync(__dirname)
