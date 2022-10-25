74959611262e681cb74bf01224228e2c539fcc58
Timothy Katee <kate2timo@gmail.com>
2022-10-26 00:15:49 +0900

    - Updated sequelize models...

---
+++ src/database/models/UserRoles.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return UserRoles.init(sequelize, DataTypes);
+}
+
+class UserRoles extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    role_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    organization_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'UserRoles',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+  }
+}
