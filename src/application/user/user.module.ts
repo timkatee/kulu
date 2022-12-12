7708e5ca9ec129f9f2dfd75680cbf18f57784705
Timothy Katee <kate2timo@gmail.com>
2022-12-13 02:14:43 +0900

    feat : Removed graphql configurations...

--- src/application/user/user.module.ts
+++ src/application/user/user.module.ts
@@ -1,21 +1,12 @@
-import {
-    ApolloFederationDriver,
-    ApolloFederationDriverConfig,
-} from '@nestjs/apollo';
 import {Module} from '@nestjs/common';
 import {GraphQLModule} from '@nestjs/graphql';
 import {UserService} from '@application/user/user.service';
 import {Repository} from "@infrastructure/database/repository"
+import {IRepository} from "@application/interfaces/repository.interface"
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloFederationDriverConfig>({
-            driver: ApolloFederationDriver,
-            autoSchemaFile: true,
-        })
-    ],
     providers: [UserService, {
-        provide: 'REPOSITORY',
+        provide: 'IRepository',
         useValue: new Repository('users')
     }],
 })
