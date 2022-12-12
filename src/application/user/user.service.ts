96455af9e8e62d2adc22949eb9a3f3f92157b33c
Timothy Katee <kate2timo@gmail.com>
2022-12-13 02:15:02 +0900

    feat : Added mutation for user creation...

--- src/application/user/user.service.ts
+++ src/application/user/user.service.ts
@@ -1,24 +1,31 @@
-import {Args, Query, Resolver, ResolveReference, PartialType, InputType} from '@nestjs/graphql/dist';
-import {Inject} from '@nestjs/common';
+import {Args, InputType, Mutation, PartialType, Query, Resolver, Scalar} from '@nestjs/graphql/dist';
 import {User} from "@domain/user/user.model"
-import {Repository} from "@infrastructure/database/repository"
+import {CrudOperations, IRepository, ReadModes} from "@application/interfaces/repository.interface";
+import {FilterInput} from "@commons/repository.filter.model";
+import {Inject} from "@nestjs/common";
 
 @InputType()
-class UserInput extends PartialType(User, InputType){
+class UserInput extends PartialType(User, InputType) {
 
 }
 
 @Resolver((of: any) => User)
 export class UserService {
-    constructor(@Inject('REPOSITORY') private repository: Repository<User>) {}
+    constructor(@Inject('IRepository') private repository: IRepository<User>) {
+    }
 
     @Query((returns) => User, {name: "User"})
-    async getUser(@Args('id') id: number): Promise<User> {
-        return await this.repository.findById(id);
+    async getUser(@Args('id') id: number): Promise<User | User[]> {
+        return await this.repository.read(ReadModes.SINGLE, {where: {id: id}});
     }
 
     @Query((returns) => [User], {name: "Users"})
-    async getUsers(@Args('filters',{nullable:true}) filters: UserInput): Promise<User[]> {
-        return await this.repository.findAll(filters);
+    async getUsers(@Args('filters', {nullable: true}) filters: FilterInput): Promise<User | User[]> {
+        return await this.repository.read(ReadModes.ALL, filters);
+    }
+
+    @Mutation((returns) => User)
+    async createEditUser(@Args('userInput',{}) userInput: UserInput,@Args('type') type: CrudOperations): Promise<User>{
+        return await this.repository.createEdit(type, userInput)
     }
 }
