595b831add502f56f7ecdb49a6386163500e3a3c
Timothy Katee <kate2timo@gmail.com>
2022-12-12 15:33:10 +0900

    fix : Fixed an issue with provider injections...

--- src/application/user/user.module.ts
+++ src/application/user/user.module.ts
@@ -2,17 +2,22 @@ import {
     ApolloFederationDriver,
     ApolloFederationDriverConfig,
 } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { UserService } from '@application/user/user.service';
+import {Module} from '@nestjs/common';
+import {GraphQLModule} from '@nestjs/graphql';
+import {UserService} from '@application/user/user.service';
+import {Repository} from "@infrastructure/database/repository"
 
 @Module({
-    providers: [UserService],
     imports: [
         GraphQLModule.forRoot<ApolloFederationDriverConfig>({
             driver: ApolloFederationDriver,
             autoSchemaFile: true,
-        }),
+        })
     ],
+    providers: [UserService, {
+        provide: 'REPOSITORY',
+        useValue: new Repository('users')
+    }],
 })
+
 export class UserModule {}
