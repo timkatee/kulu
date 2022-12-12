bb38a4135c12ef4845606287ec42665b796ed0d1
Timothy Katee <kate2timo@gmail.com>
2022-12-12 15:33:53 +0900

    feat : Added getUsers query..

--- src/application/user/user.service.ts
+++ src/application/user/user.service.ts
@@ -1,14 +1,24 @@
-import { Args, Query, Resolver, ResolveReference } from '@nestjs/graphql/dist';
+import {Args, Query, Resolver, ResolveReference, PartialType, InputType} from '@nestjs/graphql/dist';
+import {Inject} from '@nestjs/common';
 import {User} from "@domain/user/user.model"
-// import {User} from "@domain/user/user.model"
 import {Repository} from "@infrastructure/database/repository"
 
-@Resolver((of: any) => User)
-export class UserService{
-    constructor(private repository: Repository<User>) {}
+@InputType()
+class UserInput extends PartialType(User, InputType){
 
-    @Query((returns) => User)
+}
+
+@Resolver((of: any) => User)
+export class UserService {
+    constructor(@Inject('REPOSITORY') private repository: Repository<User>) {}
+
+    @Query((returns) => User, {name: "User"})
     async getUser(@Args('id') id: number): Promise<User> {
         return await this.repository.findById(id);
+    }
+
+    @Query((returns) => [User], {name: "Users"})
+    async getUsers(@Args('filters',{nullable:true}) filters: UserInput): Promise<User[]> {
+        return await this.repository.findAll(filters);
     }
 }
