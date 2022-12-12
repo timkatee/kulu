d6b206187f921b9cb3160eebf22fd12674803a95
Timothy Katee <kate2timo@gmail.com>
2022-12-12 15:32:48 +0900

    feat : Added types for graphql fields...

--- src/domain/user/user.model.ts
+++ src/domain/user/user.model.ts
@@ -1,20 +1,20 @@
 import {Entity} from "@domain/entity";
 import {IUser} from "@domain/user/user.interface"
-import {Directive, Field, ID, ObjectType} from '@nestjs/graphql/dist';
+import {Directive, Field, ID, ObjectType, Int, GraphQLISODateTime} from '@nestjs/graphql/dist';
 
 @ObjectType()
 export class User extends Entity<IUser> implements IUser {
-    @Field()
+    @Field(type => Int)
     id: number | undefined;
-    @Field()
+    @Field(type => GraphQLISODateTime)
     created_at: string | undefined;
-    @Field()
+    @Field(type => String,{nullable:true})
     full_names: string | undefined;
-    @Field()
+    @Field(type => Int)
     organization_id: number | undefined;
-    @Field()
+    @Field(type => Int)
     status: number | undefined;
-    @Field()
+    @Field(type => GraphQLISODateTime)
     updated_at: string | undefined;
 
     constructor(id: number, props: IUser) {
