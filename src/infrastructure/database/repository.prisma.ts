67ae9783dcfb6320e2a513ad636b3a7b2378d4fe
Timothy Katee <kate2timo@gmail.com>
2023-04-13 08:17:50 +0900

    fix : Updated repository to handle utilize promises effectively...

--- src/infrastructure/database/repository.prisma.ts
+++ src/infrastructure/database/repository.prisma.ts
@@ -14,24 +14,39 @@ export class RepositoryPrisma<T> implements Repository<T> {
         this.initialize().then(r => r).catch(err => this.onError(err));
     }
 
+    /**
+     * Initialize the repository and get the model attributes
+     */
     async initialize(): Promise<void> {
         this.modelAttributes = await getDataModelAttributes(this.entity)
     }
 
-    // Create a new entity
+    /**
+     * Create a new entity
+     * @param data
+     */
     async create(data: T): Promise<T> {
         // @ts-ignore
-        return await prisma[this.entity].create({data}).catch(err => this.onError(err));
+        return  prisma[this.entity].create({data}).catch(err => this.onError(err));
     }
 
-    // Update an entity by id
+    /**
+     * Update an existing entity
+     * @param id
+     * @param data
+     */
     async update(id: number, data: T): Promise<T> {
         // @ts-ignore
-        return await prisma[this.entity].update({where: {id}, data}).catch(err => this.onError(err));
+        return  prisma[this.entity].update({where: {id}, data}).catch(err => this.onError(err));
     }
 
-    // get entity
-    async readSingle(id: number, metaData: any, params: any): Promise<T> {
+    /**
+     * Get an entity by unique id
+     * @param id
+     * @param metaData
+     * @param params
+     */
+    async readSingle(id: number | string, metaData: any, params: any): Promise<T> {
         let selectFields = acquireSelectFields(acquireRequestedGraphqlFields(metaData), this.modelAttributes) || undefined
         // Todo
         // for scenarios where unique id is not {id}
@@ -40,16 +55,20 @@ export class RepositoryPrisma<T> implements Repository<T> {
             whereOverride = {where: {id: undefined, [params.idKey]: id}}
         }
         // @ts-ignore
-        return await prisma[this.entity].findUnique({...{where: {id: id}, ...{select: selectFields}, ...whereOverride}}).catch(err => this.onError(err));
+        return  prisma[this.entity].findUnique({...{where: {id: id}, ...{select: selectFields}, ...whereOverride}}).catch(err => this.onError(err));
     }
 
-    // get entities
+    /**
+     * Get many entities
+     * @param filters
+     * @param metaData
+     */
     async readMany(filters: Partial<RepositoryFilter>, metaData: any): Promise<T[]> {
         let selectFields = acquireSelectFields(acquireRequestedGraphqlFields(metaData), this.modelAttributes) || undefined
         // option to read all and override take
         try {
-            if (!filters.all) {
-                filters.take = 25
+            if (!filters.all && filters.all == false) {
+                // filters.take = 25
             } else {
                 filters.take = undefined
             }
@@ -58,26 +77,33 @@ export class RepositoryPrisma<T> implements Repository<T> {
             this.onError(e)
         }
         // @ts-ignore
-        return await prisma[this.entity].findMany({...filters, ...{select: selectFields}}).catch(err => this.onError(err));
+        return  prisma[this.entity].findMany({...filters, ...{select: selectFields}}).catch(err => this.onError(err));
     }
 
-    // Delete an entity by id
+    /**
+     * Delete an entity
+     * @param id
+     */
     async delete(id: number): Promise<T> {
         // @ts-ignore
-        return await prisma[this.entity].delete({where: {id}}).catch(err => this.onError(err));
+        return  prisma[this.entity].delete({where: {id}}).catch(err => this.onError(err));
     }
 
-    //
+    /**
+     * Create, Update or Delete an entity
+     * @param type
+     * @param data
+     */
     async createEdit(type: CrudOperations, data: T): Promise<T> {
         // @ts-ignore
         if (type == CrudOperations.CREATE) {
-            return await this.create(data)
+            return  this.create(data)
         } else if (type == CrudOperations.UPDATE) {
             // @ts-ignore
-            return await this.update(data.id, data)
+            return  this.update(data.id, data)
         } else if (type == CrudOperations.DELETE) {
             // @ts-ignore
-            return await this.delete(data.id)
+            return  this.delete(data.id)
         } else {
             this.onError({message: 'Invalid CrudOperation supplied. Allowed [CREATE, UPDATE, DELETE]'})
             return data
