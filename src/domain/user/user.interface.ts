1e5fb8e7567b24d156a0883c3c20d67ff72f2727
Turbobot <turbobot@vercel.com>
2023-05-17 13:21:36 +0900

    feat(create-turbo): apply official-starter transform

--- src/domain/user/user.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  id: number | undefined;
-  fullName: string | undefined;
-  createdAt: Date | undefined;
-  updatedAt: Date | undefined;
-  description: string | undefined;
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "@infrastructure/http/app.module";
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
-}
-
-bootstrap();
